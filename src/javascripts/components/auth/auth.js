28210622e0006b41a3bf1a5bef91e2cf059460a0
Luke Lancaster <luketlancaster@gmail.com>
2020-06-20 10:46:06 -0500

    Login button prints to the dom, and event fires

---
+++ src/javascripts/components/auth/auth.js
@@ -0,0 +1,13 @@
+import utils from '../../helpers/utils';
+
+const signMeIn = () => {
+  console.warn('Yeah I\'ll sign you in, just give me a minute!');
+};
+
+const loginButton = () => {
+  const domString = '<button id="google-auth" class="btn btn-warning"><i class="fab fa-google-plus"></i> LOG ME IN!!!</button>';
+  utils.printToDom('#auth', domString);
+  $('#google-auth').click(signMeIn);
+};
+
+export default { loginButton };
--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,7 +1,17 @@
+import auth from './components/auth/auth';
+
 import '../styles/main.scss';
 
+/**
+ * Be able to 'log in' to our app
+ * Be able to 'log out' of our app
+ * See a login button if we're NOT logged in
+ * See a logout button if we ARE logged in
+ * See a list of mushrooms (like a forest!) if we ARE logged in
+ */
+
 const init = () => {
-  console.warn('here is a thing');
+  auth.loginButton();
 };
 
 init();
