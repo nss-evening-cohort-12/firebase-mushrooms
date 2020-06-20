4cd2f09f7a94d61b3407d2cf225d9f2eb884d196
Luke Lancaster <luketlancaster@gmail.com>
2020-06-20 11:57:33 -0500

    Auth fully works

--- src/javascripts/components/auth/auth.js
+++ src/javascripts/components/auth/auth.js
@@ -1,7 +1,11 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
 import utils from '../../helpers/utils';
 
 const signMeIn = () => {
-  console.warn('Yeah I\'ll sign you in, just give me a minute!');
+  const googleProvider = new firebase.auth.GoogleAuthProvider();
+  firebase.auth().signInWithPopup(googleProvider);
 };
 
 const loginButton = () => {
--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,17 +1,17 @@
+import firebase from 'firebase/app';
+import apiKeys from './helpers/apiKeys.json';
+
 import auth from './components/auth/auth';
+import myNavbar from './components/myNavbar/myNavbar';
+import authData from './helpers/data/authData';
 
 import '../styles/main.scss';
 
-/**
- * Be able to 'log in' to our app
- * Be able to 'log out' of our app
- * See a login button if we're NOT logged in
- * See a logout button if we ARE logged in
- * See a list of mushrooms (like a forest!) if we ARE logged in
- */
-
 const init = () => {
+  firebase.initializeApp(apiKeys.firebaseConfig);
+  authData.checkLoginStatus();
   auth.loginButton();
+  myNavbar.logoutEvent();
 };
 
 init();
